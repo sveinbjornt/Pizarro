af1bb67093380b3f73bcd1076e7987603413635d
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2012-02-06 02:27:38 +0000

    Optimized images, Reward class

---
+++ Classes/Reward.h
@@ -0,0 +1,13 @@
+//
+//  Reward.h
+//  Pizarro
+//
+//  Created by Sveinbjorn Thordarson on 2/2/12.
+//  Copyright 2012 Corrino Software. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Reward : CCSprite
+
+@end
---
+++ Classes/Reward.m
@@ -0,0 +1,23 @@
+//
+//  Reward.m
+//  Pizarro
+//
+//  Created by Sveinbjorn Thordarson on 2/2/12.
+//  Copyright 2012 Corrino Software. All rights reserved.
+//
+
+#import "Reward.h"
+
+@implementation Reward
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
